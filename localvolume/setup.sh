be677fce24cfeba2cb0c2c24a597475814a3ae4d
Yecheng Fu <cofyc.jackson@gmail.com>
2018-03-29 20:05:05 +0800

    Add setup script.

---
+++ localvolume/setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+action=${1:-setup}
+
+function usage() {
+    echo "Usage: $(basename $0) [setup|teardown]"
+    echo ""
+}
+
+case $action in
+    "setup" | "teardown")
+        ;;
+    *)
+        usage
+        ;;
+esac
+
+test -d /mnt/disks || mkdir /mnt/disks
+for i in $(seq 1 100); do
+    vol=vol$i
+    dir=/mnt/disks/$vol
+    if [[ "$action" == "setup" ]]; then
+        test -d $dir || mkdir $dir
+        mount -t tmpfs $vol -osize=10240000 $dir
+    else
+        test -d $dir && umount $dir
+        test -d $dir && rmdir $dir
+    fi
+done
