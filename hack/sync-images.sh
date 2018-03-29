f95be0de811b33152fccde54fa5b4b530fe26fb9
Yecheng Fu <cofyc.jackson@gmail.com>
2018-03-29 16:44:44 +0800

    Because network latency is huge to pull images on gcr.io/quay.io. I wrote a script to sync images via a public repository.

---
+++ hack/sync-images.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+#
+# This script is used to sync images via a public registry.
+#
+
+images=(
+    gcr.io/google_containers/kube-apiserver-amd64:v1.9.6
+    gcr.io/google_containers/kube-proxy-amd64:v1.9.6
+    gcr.io/google_containers/kube-controller-manager-amd64:v1.9.6
+    gcr.io/google_containers/kube-scheduler-amd64:v1.9.6
+    gcr.io/google_containers/etcd-amd64:3.1.11
+    gcr.io/google_containers/pause-amd64:3.0
+    gcr.io/google_containers/pause-amd64:3.1
+    gcr.io/google_containers/k8s-dns-sidecar-amd64:1.14.7
+    gcr.io/google_containers/k8s-dns-kube-dns-amd64:1.14.7
+    gcr.io/google_containers/k8s-dns-dnsmasq-nanny-amd64:1.14.7
+    # ingress
+    gcr.io/google_containers/defaultbackend:1.0
+    gcr.io/google_containers/nginx-ingress-controller:0.9.0-beta.15
+    # for e2e
+    gcr.io/kubernetes-e2e-test-images/serve-hostname-amd64:1.0
+    gcr.io/kubernetes-e2e-test-images/mounttest-amd64:1.0
+    # prometheus
+    quay.io/prometheus/prometheus:v2.2.1
+)
+
+function get-normalized-image() {
+    tr -s '/' '-' <<< "$1"
+}
+
+function get-full-image() {
+    printf "reg.qiniu.com/sync/%s" $(get-normalized-image $1)
+}
+
+trap "kill 0" EXIT
+
+PIPE=$(mktemp -u)
+mkfifo $PIPE
+exec 99<>$PIPE
+rm $PIPE
+
+if [ -n "${DOCKER_USERNAME}" -a -n "${DOCKER_PASSWORD}" ]; then
+    docker login reg.qiniu.com -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}
+else
+    docker login reg.qiniu.com
+fi
+
+(
+for img in ${images[*]}; do
+    (
+    echo "====== syncing $img" >&99
+    rurl=$img
+    lurl=$(get-full-image $img)
+    docker pull $rurl
+    docker tag $rurl $lurl
+    docker push $lurl
+    echo "====== syncing $img done" >&99
+    ) 0>&- 1>&- &
+done
+wait
+echo "done" >&99
+) &
+
+(
+	while IFS= read -r line; do
+		[[ "$line" == "done" ]] && break
+		echo "$line"
+	done <&99
+) &
+
+wait
+
+echo "Now you can use following commands to get all images:"
+
+for img in ${images[*]}; do
+    rurl=$img
+    lurl=$(get-full-image $img)
+    echo docker pull $lurl
+    echo docker tag $lurl $rurl
+done
