58feb3df8de13331eb20405d7b41db40e3f4dc29
Yecheng Fu <cofyc.jackson@gmail.com>
2018-09-18 12:21:57 +0800

    more issues

---
+++ issues/68391/test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# See https://github.com/kubernetes/community/blob/master/contributors/devel/testing.md#integration-tests.
+#
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+KUBE_ROOT=$GOPATH/src/k8s.io/kubernetes
+cd $KUBE_ROOT
+
+go test k8s.io/kubernetes/pkg/controller/volume/persistentvolume
+make test-integration WHAT=./test/integration/scheduler GOFLAGS="-v" KUBE_TEST_VMODULE="scheduler*=5,pv*=5,factory*=5,equivalence*=5" KUBE_TEST_ARGS="-run ." 
